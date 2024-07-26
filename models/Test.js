2cfacb50ed0376ef7460c978bb7ed22c0c88e60c
Kateryna Sosnyuk <54499224+katja-droid@users.noreply.github.com>
2024-07-26 16:44:52 +0300

    Update Test.js

--- models/Test.js
+++ models/Test.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// Define the schema for the questions with an added heading property
 const testQuestionSchema = new Schema({
   heading: { type: String, required: true },
   type: { type: String, enum: ['multiple_choice', 'true_false', 'fill_in_the_blank'], required: true },
@@ -10,21 +9,18 @@ const testQuestionSchema = new Schema({
   correctAnswer: { type: String, required: true },
 });
 
-// Define the schema for the topics without a YouTube link
 const testTopicSchema = new Schema({
   topicName: { type: String, required: true },
   information: { type: String, required: true },
   questions: [testQuestionSchema],
 });
 
-// Define the main test schema
 const testSchema = new Schema({
   testName: { type: String, required: true },
   imageUri: { type: String, required: true },
   topics: [testTopicSchema]
 });
 
-// Compile and export our model
 const Test = mongoose.model('Test', testSchema);
 
 module.exports = Test;
